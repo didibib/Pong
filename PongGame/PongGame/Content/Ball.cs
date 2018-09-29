e8dd4f448ea0d1cdb0011a07000e505ddb9bd3ad
Jase <jaykruissink@live.nl>
2018-09-29 23:39:49 +0200

    Comments added for code & obsolete controls button removed

--- PongGame/PongGame/Content/Ball.cs
+++ PongGame/PongGame/Content/Ball.cs
@@ -16,28 +16,34 @@ namespace PongGame.Content
         private float minSpeed;
         private float friction = 0.01f;
         public string side;
-        
+        private Random random = new Random();
+        public Player ActivePlayer;
 
-        public Ball(Texture2D texture, Vector2 position, int direction, float startSpeed) : base(texture, startSpeed) {
-            StartDirection(direction);
+
+        public Ball(Texture2D texture, Vector2 position, float startSpeed = 3f) : base(texture, startSpeed) {
+            StartDirection();
             speed = minSpeed = startSpeed;
             spriteSpeed = .2f;
             Position = position;
         }
+
         public void resetPotition()
         {
+            //Resets the ball to the middle of the screen
             Position = new Vector2(graphics.Viewport.Width / 2, graphics.Viewport.Height / 2);
+            speed = minSpeed;
         }
 
         public void Move(GameTime gameTime)
         {
-            if (Position.X <= 0 + Rectangle.Width / 2) // Player 1
+            //Hit Detection with walls, calls callPlayer() method for each wall
+            if (Position.X <= 0 + Rectangle.Width / 2) // Left wall, Player 1
             { Direction.X *= -1; Game1.callPlayer(1); }
-            if (Position.X >= graphics.Viewport.Width - Rectangle.Width / 2) // Player 2
+            if (Position.X >= graphics.Viewport.Width - Rectangle.Width / 2) // Right Wall, Player 2
             { Direction.X *= -1; Game1.callPlayer(2); }
-            if (Position.Y <= 0 + Rectangle.Height / 2) // Player 3
+            if (Position.Y <= 0 + Rectangle.Height / 2) // Upper wall, Player 3
             { Direction.Y *= -1; Game1.callPlayer(3); }
-            if (Position.Y >= graphics.Viewport.Height - Rectangle.Height / 2) // Player 4
+            if (Position.Y >= graphics.Viewport.Height - Rectangle.Height / 2) // Lower wall, Player 4
             { Direction.Y *= -1; Game1.callPlayer(4); }
 
 
@@ -49,21 +55,42 @@ namespace PongGame.Content
 
         public void AddSpeed(float increment) {
             speed += increment;            
-        }       
+        }
 
-        private void StartDirection(int d) {
+        public void CheckCollisionPlayer(Player target) {
+            if (Rectangle.Intersects(target.Rectangle)) {
+                if (Rectangle.Bottom > target.Rectangle.Top && Rectangle.Bottom < target.Rectangle.Bottom ||
+                    Rectangle.Top > target.Rectangle.Top && Rectangle.Top < target.Rectangle.Top ||
+                    Rectangle.Right < target.Rectangle.Right && Rectangle.Right > target.Rectangle.Left) {
+                    Direction = target.Direction;
+                    AddSpeed(target.spriteSpeed);
+                    SetPlayer(target);
+                }
+                else if (Rectangle.Bottom > target.Rectangle.Bottom || Rectangle.Top < target.Rectangle.Top)
+                    Direction.X *= -1;
+                else if (Rectangle.Right < target.Rectangle.Left || Rectangle.Left > target.Rectangle.Right)
+                    Direction.Y *= -1;
+            }
+        }
+
+        public void SetPlayer(Player player) {
+            ActivePlayer = player;
+        }
+
+        private void StartDirection() {
+            int d = random.Next(0, 3);
             switch (d) {
                 case 0:
-                    Direction = new Vector2(1, 1);
+                    Direction = new Vector2(.5f, .5f);
                     break;
                 case 1:
-                    Direction = new Vector2(-1, 1);
+                    Direction = new Vector2(-.5f, .5f);
                     break;
                 case 2:
-                    Direction = new Vector2(1, -1);
+                    Direction = new Vector2(.5f, -.5f);
                     break;
                 case 3:
-                    Direction = new Vector2(-1, -1);
+                    Direction = new Vector2(-.5f, -.5f);
                     break;
             }
         }
--- PongGame/PongGame/PongGame.cs
+++ PongGame/PongGame/PongGame.cs
@@ -27,9 +27,8 @@ namespace PongGame
 
 
         List<Player> Players;
-        Ball Ball;
-
-        float ballSpeed = 3f;
+        public List<Ball> Balls;
+        public PowerUps PowerUps;
 
         public Game1() {
             graphics = new GraphicsDeviceManager(this);
@@ -40,12 +39,10 @@ namespace PongGame
             #region GamePlay
             random = new Random();
             Sprites = new List<Sprite>();
-            Ball = new Ball(
-                Content.Load<Texture2D>("Sprites/Ball"),
-                new Vector2(graphics.GraphicsDevice.Viewport.Width / 2, graphics.GraphicsDevice.Viewport.Height / 2),
-                random.Next(0, 3),
-                ballSpeed);
-            Sprites.Add(Ball);
+            Balls = new List<Ball>() {
+                new Ball(Content.Load<Texture2D>("Sprites/Ball"),
+                new Vector2(graphics.GraphicsDevice.Viewport.Width / 2, graphics.GraphicsDevice.Viewport.Height / 2))
+            };
 
             #region Players
             Players = new List<Player>() {
@@ -99,33 +96,42 @@ namespace PongGame
                 },
                 "Player4")
             };
+            //ActivePlayers list for keeping track of active players
             ActivePlayers = new List<Player>();
-            //Sprites.AddRange(Players);
             #endregion
             #endregion
-
+            PowerUps = new PowerUps(10,
+                Content.Load<Texture2D>("Sprites/ExtraBall"),
+                Content.Load<Texture2D>("Sprites/ExtraHeart")
+                );
             base.Initialize();
         }
 
         protected override void LoadContent() {
+            //Loading content to be used
             TwoPlayersBtn = this.Content.Load<Texture2D>("Sprites/Buttons/2PlayersBtn");
             ThreePlayersBtn = this.Content.Load<Texture2D>("Sprites/Buttons/3PlayersBtn");
             FourPlayersBtn = this.Content.Load<Texture2D>("Sprites/Buttons/4PlayersBtn");
-            ControlsBtn = this.Content.Load<Texture2D>("Sprites/Buttons/ControlsBtn");
             ResetBtn = this.Content.Load<Texture2D>("Sprites/Buttons/ResetBtn");
             StartBtn = this.Content.Load<Texture2D>("Sprites/Buttons/StartBtn");
-            font = Content.Load<SpriteFont>("Arial");
             font = Content.Load<SpriteFont>("Arial");
             spriteBatch = new SpriteBatch(GraphicsDevice);
         }
 
         protected override void UnloadContent() {
+            //Disposing loaded content 
+            TwoPlayersBtn.Dispose();
+            ThreePlayersBtn.Dispose();
+            FourPlayersBtn.Dispose();
+            ResetBtn.Dispose();
             StartBtn.Dispose();
+            font.Dispose();
+            spriteBatch.Dispose();
         }
 
-        public static void callPlayer(int x)
-        {
-            switch(x) {
+        public static void callPlayer(int x) {
+            //Recieves calls from Ball and sets string wantedPlayer
+            switch (x) {
                 case 1:
                     wantedPlayer = "Player1";
                     break;
@@ -141,77 +147,66 @@ namespace PongGame
             }
         }
 
-        public void LoseLives(string x)
-        {
-            for (int i = ActivePlayers.Count; i --> 0;) {
-                if (ActivePlayers[i].name == x)
-                {
+        public void LoseLives(string x) {
+            for (int i = ActivePlayers.Count; i-- > 0;) {
+                if (ActivePlayers[i].name == x) {
+                    //For every player with the same name as the wantedPlayer string, a life is removed
                     ActivePlayers[i].lives--;
-                    if (ActivePlayers[i].lives <= 0)
-                    {
+                    if (ActivePlayers[i].lives <= 0) {
+                        //If their lives reach zero, they are no longer active, thus removed from the ActivePlayers list
                         ActivePlayers.RemoveAt(i);
                     }
-                    Ball.resetPotition();
+                    //Extra ball is reset and the reset funtion is called for the original ball
+                    Balls.RemoveRange(1, Balls.Count-1);
+                    Balls[0].resetPotition();
                 }
             }
-            
-            if (ActivePlayers.Count == 1)
-            {
+
+            if (ActivePlayers.Count == 1) {
+                //If only 1 player is active, this player has won and the GameOver sequence is started.
                 gameStatus = GameStatus.GameOver;
             }
         }
 
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed 
+        protected override void Update(GameTime gameTime) {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
                 || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            switch (gameStatus)
-            {
+            switch (gameStatus) {
                 case GameStatus.BootMenu:
+                    //Quickly setting Mouse button visibility after startup/restart
                     while (this.IsMouseVisible == false) { this.IsMouseVisible = true; }
+                    //Button to start the game (Copy Players list to ActivePlayers, changing Game Status from BootMenu to PlayerSelect)
                     #region StartBtn
-                    if (Mouse.GetState().LeftButton == ButtonState.Pressed 
+                    
+                    if (Mouse.GetState().LeftButton == ButtonState.Pressed
                         & Mouse.GetState().X >= (graphics.GraphicsDevice.Viewport.Width - StartBtn.Width) / 2
-                        & Mouse.GetState().X <= (graphics.GraphicsDevice.Viewport.Width + StartBtn.Width) / 2)
-                    {
+                        & Mouse.GetState().X <= (graphics.GraphicsDevice.Viewport.Width + StartBtn.Width) / 2) {
                         if (Mouse.GetState().Y >= (graphics.GraphicsDevice.Viewport.Height - StartBtn.Height) / 5 * 3
-                            & Mouse.GetState().Y <= (graphics.GraphicsDevice.Viewport.Height + StartBtn.Height) / 5 * 3)
-
-                        { ActivePlayers.AddRange(Players); gameStatus = GameStatus.PlayerSelect; }
-
-                        if (Mouse.GetState().Y >= (graphics.GraphicsDevice.Viewport.Height - StartBtn.Height) / 5 * 4
-                            & Mouse.GetState().Y <= (graphics.GraphicsDevice.Viewport.Height + StartBtn.Height) / 5 * 4)
-
-                        {
-                            //Controls
-                        }
+                            & Mouse.GetState().Y <= (graphics.GraphicsDevice.Viewport.Height + StartBtn.Height) / 5 * 3) { ActivePlayers.AddRange(Players); gameStatus = GameStatus.PlayerSelect; }
                     }
                     #endregion
                     break;
 
                 case GameStatus.PlayerSelect:
+                    // Selecting the desired amount of players (Remove unnecessary players with RemoveRange() and setting Game Status to GamePlay)
                     #region SelectBtns
-                    if (Mouse.GetState().LeftButton == ButtonState.Pressed 
+                    if (Mouse.GetState().LeftButton == ButtonState.Pressed
                         & Mouse.GetState().X >= (graphics.GraphicsDevice.Viewport.Width - StartBtn.Width) / 2
-                        & Mouse.GetState().X <= (graphics.GraphicsDevice.Viewport.Width + StartBtn.Width) / 2)
-                    {
+                        & Mouse.GetState().X <= (graphics.GraphicsDevice.Viewport.Width + StartBtn.Width) / 2) {
                         if (Mouse.GetState().Y >= (graphics.GraphicsDevice.Viewport.Height - StartBtn.Height) / 4
-                            & Mouse.GetState().Y <= (graphics.GraphicsDevice.Viewport.Height + StartBtn.Height) / 4)
-                        {
+                            & Mouse.GetState().Y <= (graphics.GraphicsDevice.Viewport.Height + StartBtn.Height) / 4) {
                             ActivePlayers.RemoveRange(2, 2);
                             gameStatus = GameStatus.GamePlay;
                         }
                         else if (Mouse.GetState().Y >= (graphics.GraphicsDevice.Viewport.Height - StartBtn.Height) / 2
-                            & Mouse.GetState().Y <= (graphics.GraphicsDevice.Viewport.Height + StartBtn.Height) / 2)
-                        {
+                            & Mouse.GetState().Y <= (graphics.GraphicsDevice.Viewport.Height + StartBtn.Height) / 2) {
                             ActivePlayers.RemoveRange(3, 1);
                             gameStatus = GameStatus.GamePlay;
                         }
                         else if (Mouse.GetState().Y >= (graphics.GraphicsDevice.Viewport.Height - StartBtn.Height) / 4 * 3
-                            & Mouse.GetState().Y <= (graphics.GraphicsDevice.Viewport.Height + StartBtn.Height) / 4 * 3)
-                        {
+                            & Mouse.GetState().Y <= (graphics.GraphicsDevice.Viewport.Height + StartBtn.Height) / 4 * 3) {
                             gameStatus = GameStatus.GamePlay;
                         }
                     }
@@ -220,36 +215,35 @@ namespace PongGame
 
                 case GameStatus.GamePlay:
                     #region GamePlay
-                    if (wantedPlayer != null)
-                    {
+                    if (wantedPlayer != null) {
+                        // If A wanted player is set in the callPlayer() method, the LoseLives() function is started for this player.
                         LoseLives(wantedPlayer);
                         wantedPlayer = null;
                     }
                     this.IsMouseVisible = false;
-                    for (int i = 0; i < Sprites.Count; i++)
-                        if (Sprites[i] is Ball)
-                            {
-                                for (int j = 0; j < ActivePlayers.Count; j++)
-                                {
-                                        Ball.CheckCollision(ActivePlayers[j], Ball);
-                                }
-                            }
-                    foreach (Player p in Players)
-                    {
+                    for (int i = 0; i < Balls.Count; i++)
+                        for (int j = 0; j < ActivePlayers.Count; j++) {
+                            Balls[i].CheckCollisionPlayer(ActivePlayers[j]);
+                        }
+                    foreach (Player p in Players) {
                         p.Move(gameTime);
                     }
-                    Ball.Move(gameTime);
+                    foreach (Ball b in Balls)
+                        b.Move(gameTime);
+                    PowerUps.Update(gameTime);
+                    PowerUps.CheckBallCollision(Balls);
+                    
                     #endregion
                     break;
                 case GameStatus.GameOver:
+                    //Reset button for resetting the game to the Boot Menu and resetting all player lives to 3
                     #region GameOver
                     while (this.IsMouseVisible == false) { this.IsMouseVisible = true; }
                     if (Mouse.GetState().LeftButton == ButtonState.Pressed
                         & Mouse.GetState().X >= (graphics.GraphicsDevice.Viewport.Width - StartBtn.Width) / 2
                         & Mouse.GetState().X <= (graphics.GraphicsDevice.Viewport.Width + StartBtn.Width) / 2
                         & Mouse.GetState().Y >= (graphics.GraphicsDevice.Viewport.Height - StartBtn.Height) / 2
-                        & Mouse.GetState().Y <= (graphics.GraphicsDevice.Viewport.Height + StartBtn.Height) / 2)
-                    {
+                        & Mouse.GetState().Y <= (graphics.GraphicsDevice.Viewport.Height + StartBtn.Height) / 2) {
                         ActivePlayers.AddRange(Players);
                         for (int i = ActivePlayers.Count; i-- > 0;) { ActivePlayers[i].lives = 3; }
                         gameStatus = GameStatus.BootMenu;
@@ -264,19 +258,17 @@ namespace PongGame
             GraphicsDevice.Clear(bgColor);
 
             spriteBatch.Begin();
-            switch (gameStatus)
-            {
+            switch (gameStatus) {
                 case GameStatus.BootMenu:
+                    //Draws Start button in the middle of the screen at 3/5 the screen length
                     #region DrwStartBtns
                     spriteBatch.Draw(StartBtn, destinationRectangle: new Rectangle(
                         (graphics.GraphicsDevice.Viewport.Width - StartBtn.Width) / 2,
                         (graphics.GraphicsDevice.Viewport.Height - StartBtn.Height) / 5 * 3, StartBtn.Width, StartBtn.Height));
-                    spriteBatch.Draw(ControlsBtn, destinationRectangle: new Rectangle(
-                        (graphics.GraphicsDevice.Viewport.Width - StartBtn.Width) / 2,
-                        (graphics.GraphicsDevice.Viewport.Height - StartBtn.Height) / 5 * 4, StartBtn.Width, StartBtn.Height));
                     #endregion
                     break;
                 case GameStatus.PlayerSelect:
+                    //Draws Player Select Buttons in the middle of the screen at 1/4th, 1/2th and 3/4th the screen length
                     #region DrwStartBtns2
                     spriteBatch.Draw(TwoPlayersBtn, destinationRectangle: new Rectangle(
                         (graphics.GraphicsDevice.Viewport.Width - StartBtn.Width) / 2,
@@ -293,8 +285,11 @@ namespace PongGame
                 case GameStatus.GamePlay:
                     foreach (Sprite sprite in Sprites) { sprite.Draw(spriteBatch); }
                     foreach (Sprite sprite in ActivePlayers) { sprite.Draw(spriteBatch); }
+                    foreach(Sprite sprite in Balls) { sprite.Draw(spriteBatch); }
+                    PowerUps.Draw(spriteBatch);
                     break;
                 case GameStatus.GameOver:
+                    //Draws winned statement in the bottom left and reset button in the middle of the screen
                     spriteBatch.DrawString(font, ActivePlayers[0].name.ToString() + " has won!", new Vector2(30, graphics.GraphicsDevice.Viewport.Height - 50), Color.Black);
                     spriteBatch.Draw(ResetBtn, destinationRectangle: new Rectangle(
                         (graphics.GraphicsDevice.Viewport.Width - StartBtn.Width) / 2,
